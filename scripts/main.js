69e25cdf084caf36f34454d687d32fa6ac00168c
reyes25 <elcreyes@berkeley.edu>
2015-12-01 01:07:05 -0800

    Another commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
 	var height = $(window).height();
+	console.log('height:', height);
 	$('#left, #right, #center').css('height', height - 25); 
 	$('#view2').css('height', height);
 
