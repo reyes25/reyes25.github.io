1e34a206f318492c6719c4100c65f11196b59615
reyes25 <elcreyes@berkeley.edu>
2015-12-01 00:26:17 -0800

    Display fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	
+	var height = $(window).height();
+	$('#left, #right, #center').css('height', height);
+
 	$('#nav-beverages').click(function() {
 		$('html,body').animate({
     		scrollTop: $('#view2').offset().top
