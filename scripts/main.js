25ff87d3e0f9e906210c5eea112d84ece9a6b605
reyes25 <elcreyes@berkeley.edu>
2015-12-01 23:51:26 -0800

    Another commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+
+	$('.nav li span').hover(function() {
+		$(this).addClass('nav-li-hover');
+	}, function() {
+		$(this).removeClass('nav-li-hover');
+	});
 	
 	var height = $(window).height();
-	console.log('height:', height);
+		width = $(window).width();
+
 	$('#left, #right, #center').css('height', height - 80); 
 	$('#view2').css('height', height);
 
@@ -9,6 +16,22 @@ $(document).ready(function() {
 		$('html,body').animate({
     		scrollTop: $('#view2').offset().top
     	}, 500);
+	});
+
+	$('#modal').css({
+		top: height/2 - 200,
+		left:width/2 - 300
+	});
+
+
+	$('#modal-overlay').fadeIn(500, function() {
+		$('#modal').slideDown(500, function() {
+
+		});
+	});
+
+	$('button').on('click', function(event) {
+		$('#modal-overlay').fadeOut();
 	});
 
 });
